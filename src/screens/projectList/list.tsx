1959413e96fe09f8916e534b4b5d7218e859f64e
sticla <yuanzichao@163.com>
2021-08-04 22:17:18 +0800

    feat: use antd
    使用antd组件库重构登录注册模块，添加craco修改app默认配置

--- src/screens/projectList/list.tsx
+++ src/screens/projectList/list.tsx
@@ -1,28 +1,32 @@
 import { Project, User } from "./index";
+import { Table } from "antd";
 interface ListProp {
   list: Project[];
   users: User[];
 }
 export const List = ({ list, users }: ListProp) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>项目名称</th>
-          <th>负责人</th>
-        </tr>
-      </thead>
-      <tbody>
-        {list.map((project) => (
-          <tr key={project.id}>
-            <td>{project.name}</td>
-            <td>
-              {users.find((user) => user.id === project.personId)?.name ||
-                "未知"}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <Table
+      pagination={false}
+      columns={[
+        {
+          title: "名称",
+          dataIndex: "name",
+          sorter: (a, b) => a.name.localeCompare(b.name),
+        },
+        {
+          title: "负责人",
+          render(value, project) {
+            return (
+              <span>
+                {users.find((user) => user.id === project.personId)?.name ||
+                  "未知"}
+              </span>
+            );
+          },
+        },
+      ]}
+      dataSource={list}
+    ></Table>
   );
 };
