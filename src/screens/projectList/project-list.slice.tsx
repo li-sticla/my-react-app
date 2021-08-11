42dc0bac08eed275ed3adcfba621bd0aa98e616a
li-sticla <yuanzichao@163.com>
2021-08-12 01:39:42 +0800

    feat: use react-redux

---
+++ src/screens/projectList/project-list.slice.tsx
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+interface State {
+  projectModalOpen: boolean;
+}
+
+const initialState: State = {
+  projectModalOpen: false,
+};
+
+export const projectListSlice = createSlice({
+  name: "projectListSlice",
+  initialState,
+  reducers: {
+    openProjectModal(state) {
+      // toolskit用immer库处理映射，此时操作的并非传入的state而是创建新的state并返回
+      state.projectModalOpen = true;
+    },
+    closeProjectModal(state) {
+      state.projectModalOpen = false;
+    },
+  },
+});
+
+export const projectListActions = projectListSlice.actions;
---
+++ src/store/index.tsx
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const rootReducer = {};
+export const store = configureStore({
+  reducer: rootReducer,
+});
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
